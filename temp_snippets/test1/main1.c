d9e806389c2e00be35986d63dc09207a25257c12
yonka <252941898@qq.com>
2015-12-30 14:15:47 +0800

    ...

---
+++ temp_snippets/test1/main1.c
@@ -0,0 +1,22 @@
+/* main.c */
+#include <stdio.h>
+
+void push(char);
+char pop(void);
+int is_empty(void);
+
+int a, b = 1;
+
+int main(void)
+{
+    push('a');
+    push('b');
+    push('c');
+
+    while(!is_empty())
+        putchar(pop());
+    putchar('\n');
+
+    return 0;
+}
+
